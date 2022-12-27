ae4f3d5749717d37e433e905cd2e43d2fa2fbf8c
Jeonwoojae <dnwo0719@icloud.com>
2022-12-27 18:13:26 +0900

    Init project

---
+++ study_http/src/test/java/com/example/study_http/StudyHttpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.study_http;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyHttpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
