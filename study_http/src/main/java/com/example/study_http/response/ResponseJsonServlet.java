3751109d03c98d3eb03b3e693dc13fb3807682fd
Jeonwoojae <dnwo0719@icloud.com>
2022-12-27 21:54:42 +0900

    response objectMapper로 json 반환하기

---
+++ study_http/src/main/java/com/example/study_http/response/ResponseJsonServlet.java
@@ -0,0 +1,34 @@
+package com.example.study_http.response;
+
+import com.example.study_http.HelloData;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * http://localhost:8080/response-json
+ */
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //Content-Type: application/json
+        resp.setHeader("content-type", "application/json");
+        resp.setCharacterEncoding("utf-8");
+
+        HelloData data = new HelloData();
+        data.setUsername("kim");
+        data.setAge(20);
+
+        //{"username":"kim","age":20}
+        String result = objectMapper.writeValueAsString(data);
+        resp.getWriter().write(result);
+    }
+}
