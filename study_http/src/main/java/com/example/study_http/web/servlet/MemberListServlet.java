dae18d8bf7fbc0b171ef4eaceb90ab18de1b6edf
Jeonwoojae <dnwo0719@icloud.com>
2022-12-29 13:47:27 +0900

    서블릿으로 회원관리 기능, 페이지 만들기

---
+++ study_http/src/main/java/com/example/study_http/web/servlet/MemberListServlet.java
@@ -0,0 +1,56 @@
+package com.example.study_http.web.servlet;
+
+import com.example.study_http.domain.Member;
+import com.example.study_http.domain.MemberRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+/**
+ * 1.   memberRepository.findAll() 을통해 모든회원을 조회한다.
+ * 2.  회원목록 HTML을 for 루프를통해서 회원수 만큼동적으로생성하고응답한다.
+ */
+@WebServlet(name = "memberListServlet", urlPatterns = "/servlet/members")
+public class MemberListServlet extends HttpServlet {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+        List<Member> members = memberRepository.findAll();
+        PrintWriter w = resp.getWriter();
+        w.write("<html>");
+        w.write("<head>");
+        w.write("    <meta charset=\"UTF-8\">");
+        w.write("    <title>Title</title>");
+        w.write("</head>");
+        w.write("<body>");
+        w.write("<a href=\"/index.html\">메인</a>");
+        w.write("<table>");
+        w.write("    <thead>");
+        w.write("    <th>id</th>");
+        w.write("    <th>username</th>");
+        w.write("    <th>age</th>");
+        w.write("    </thead>");
+        w.write("    <tbody>");
+
+        for (Member member : members) {
+            w.write("    <tr>");
+            w.write("        <td>" + member.getId() + "</td>");
+            w.write("        <td>" + member.getUsername() + "</td>");
+            w.write("        <td>" + member.getAge() + "</td>");
+            w.write("    </tr>");
+        }
+        w.write("    </tbody>");
+        w.write("</table>");
+        w.write("</body>");
+        w.write("</html>");
+    }
+}
