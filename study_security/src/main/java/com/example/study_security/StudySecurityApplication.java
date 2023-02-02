aaa50d6109885a567c68800a72866b7e04dd8bcb
Jeonwoojae <dnwo0719@icloud.com>
2023-02-02 10:08:49 +0900

    security 공부 시작

---
+++ study_security/src/main/java/com/example/study_security/StudySecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.study_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySecurityApplication.class, args);
+    }
+
+}
