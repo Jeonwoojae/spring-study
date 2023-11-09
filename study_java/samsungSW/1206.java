5f34b6142548b52881fa79fe91b9e772679d7294
Jeonwoojae <dnwo0719@icloud.com>
2023-11-10 00:31:42 +0900

    D3 view - 윈도우 슬라이딩 속도 개선

--- study_java/samsungSW/1206.java
+++ study_java/samsungSW/1206.java
@@ -2,43 +2,25 @@ import java.util.Scanner;
 import java.io.FileInputStream;
 import java.util.*;
 
-class Solution
-{
-    public static void main(String args[]) throws Exception
-    {
+class Solution {
+    public static void main(String args[]) throws Exception {
         Scanner sc = new Scanner(System.in);
-        int T;
-        T=sc.nextInt();
-
-
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
-            int buildingCount = sc.nextInt();
-            Integer[] buildingList = new Integer[buildingCount+2];
-            for(int i=0; i<buildingCount; i++) {
-                int height = sc.nextInt();
-                buildingList[i] = height;
-            }
-            buildingList[buildingCount] = 0;
-            buildingList[buildingCount+1] = 0;
-
-
-            int result = 0;
-            Integer[] window = {0,0,0,buildingList[0],buildingList[1]};
-            for(int i=2;i<buildingCount;i++) {
-                window[0] = window[1];
-                window[1] = window[2];
-                window[2] = window[3];
-                window[3] = window[4];
-                window[2] = buildingList[i];
-
-                int maxHeight = Math.max(Math.max(window[0], window[1]), Math.max(window[3], window[4]));
-                if (maxHeight < window[2]) {
-                    result = result + window[2] - maxHeight;
-                }
+        int T = 10;
+        for (int tc = 1; tc <= T; tc++) {
+            int N = sc.nextInt(); // 가로 길이
+            int[] building = new int[N];
+            for (int i = 0; i < N; i++) {
+                building[i] = sc.nextInt();
             }
 
-            System.out.println("#"+T +" " + result);
-        }
+            int cnt = 0;
+            for (int i = 2; i < N - 2; i++) {
+                int max = Math.max(building[i - 2], Math.max(building[i - 1], Math.max(building[i + 1], building[i + 2])));
+                if (building[i] - max > 0)
+                    cnt += building[i] - max;
+            }
+
+            System.out.println("#" + tc + " " + cnt);
+        } // end of TC
     }
 }
