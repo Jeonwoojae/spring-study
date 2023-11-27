3ec99bcc3631aed1cde1ac0e38507db1034e9055
Jeonwoojae <dnwo0719@icloud.com>
2023-11-27 23:53:34 +0900

    lv2 - 원 내부 좌표 갯수 구하기(시간 초과로 수정)

--- study_java/programmers/lv2/solution3.java
+++ study_java/programmers/lv2/solution3.java
@@ -1,11 +1,14 @@
 class Solution {
     public long solution(int r1, int r2) {
         long answer = 0;
-        for (int x = r1 + 1; x < r2; x++) {
-            for (int y = 0; y*y <= x*x - r1*r1 && y*y <= r2*r2 - x*x; y++) {
-                answer++;
-            }
+
+        for (int i = 1; i <= r2; i++) {
+            int start = (int) Math.ceil(Math.sqrt((long) r1 * r1 - (long) i * i));
+            int end = (int) Math.floor(Math.sqrt((long) r2 * r2 - (long) i * i));
+
+            answer += end - start + 1;
         }
+
         return answer * 4;
     }
 }
