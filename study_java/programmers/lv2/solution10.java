e1c81929d56dc9cd877bd775861c109ce3752baf
Jeonwoojae <dnwo0719@icloud.com>
2023-12-15 00:02:01 +0900

    피보나치 수 구하기 - 모듈러 연산 사용

--- study_java/programmers/lv2/solution10.java
+++ study_java/programmers/lv2/solution10.java
@@ -12,4 +12,22 @@ class Solution {
 
         return (int) answer % 1234567;
     }
+
+    /*
+    * 2. 모듈러 연산 사용하기
+    * (A+B) mod n = ((a mod n) + (b mod n)) mod n
+    * 해당 방법을 사용하면 오버플로우를 걱정하지 않고 직접 더하여 구할 수 있다.
+    * */
+    public static int solution(int n) {
+        int a = 0;
+        int b = 1;
+
+        for (int i = 0; i < n; i++) {
+            int temp = (a + b) % 1234567;
+            a = b % 1234567;
+            b = temp % 1234567;
+        }
+
+        return a;
+    }
 }
