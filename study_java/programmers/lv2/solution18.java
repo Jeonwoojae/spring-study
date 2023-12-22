c40f09d6bb390518c1fa36ecbaa116193ab2ba94
Jeonwoojae <dnwo0719@icloud.com>
2023-12-22 22:58:15 +0900

    k개를 고를 때 총 합의 종류가 가장 적도록 구하기

---
+++ study_java/programmers/lv2/solution18.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public static int solution(int k, int[] tangerine) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int t : tangerine) {
+            map.put(t, map.getOrDefault(t, 0) + 1);
+        }
+
+        int sum = 0;
+        int ans = 0;
+        List<Integer> list = new ArrayList<>();
+        for (Integer x : map.keySet()) {
+            list.add(map.get(x));
+        }
+        list.sort(Collections.reverseOrder());
+
+        for (Integer x : list) {
+            ans++;
+            sum += x;
+            if (sum >= k) {
+                break;
+            }
+        }
+
+        return ans;
+    }
+
+}
