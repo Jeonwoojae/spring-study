f6dcb4e15c3238ce5c9ee21eecca0f3d5707122e
Jeonwoojae <dnwo0719@icloud.com>
2024-01-11 20:14:25 +0900

    정수를 1로 만들기 위한 최소 연산 - DP
    - 배열에 각 정수를 만들기 위한 최솟값 저장
    - 오래 걸리는 순으로 배열에 대응되는 값 업데이트
      - 큰수를 나눌수록, 빼기를 적게할 수록 최솟값

---
+++ study_java/Backjoon1463.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    /*
+    * 가능한 연산은 -1, /2, /3이 있다.
+    * */
+    public static int[] array;
+    public static int findMin(int a) {
+        // 1이거나 배열에 기록이 있으면 해당 값을 반환
+        if(a == 1) return 0;
+        if(array[a]>0) return array[a];
+
+        // 세가지 연산들을 수행하여 최소 값을 찾는다.
+        // 큰수로 나눌수록, -1을 적게 할 수록 최소 값이기 때문에 오래 걸리는 것 순으로 array의 값을 업데이트한다.
+        array[a] = findMin(a-1) + 1;
+        if(a%2 == 0) {
+            int temp = findMin(a/2)+1;
+            if(array[a]>temp) {
+                array[a] = temp;
+            }
+        }
+        if(a%3 == 0) {
+            int temp = findMin(a/3)+1;
+            if(array[a]>temp) {
+                array[a] = temp;
+            }
+        }
+        return array[a];
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        array = new int[a + 1];
+
+        System.out.println(findMin(a));
+    }
+}
