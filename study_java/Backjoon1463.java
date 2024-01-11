220b8ca90919aae832ad7282d10313556ae9aca1
Jeonwoojae <dnwo0719@icloud.com>
2024-01-11 21:35:19 +0900

    정수를 1로 만들기 위한 최소 연산 - DP(Bottom-Up방식)
    - 기존 방법(Top-Bottom)
      - 큰 문제를 작은 문제로 나누고,
      - 작은 문제를 해결하여 큰 문제를 해결한다.
      - 재귀 호출 사용
    - 새로운 방법(Bottom-Up)
      - 크기가 작은 문제부터 푼다.
      - 크기를 점점 크게 만들면서 풀면 큰 문제를 해결할 수 있다.
      - 반복문 사용

--- study_java/Backjoon1463.java
+++ study_java/Backjoon1463.java
@@ -1,5 +1,26 @@
 import java.util.*;
 
+// ------------------------------------------ Bottom-Up 방식
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] d = new int[n+1];
+        d[1] = 0;
+        for (int i=2; i<=n; i++) {
+            d[i] = d[i-1] + 1;
+            if (i%2 == 0 && d[i] > d[i/2] + 1) {
+                d[i] = d[i/2] + 1;
+            }
+            if (i%3 == 0 && d[i] > d[i/3] + 1) {
+                d[i] = d[i/3] + 1;
+            }
+        }
+        System.out.println(d[n]);
+    }
+}
+
+// ---------------------------------------- Top-Bottom 방식
 public class Main {
     /*
     * 가능한 연산은 -1, /2, /3이 있다.
