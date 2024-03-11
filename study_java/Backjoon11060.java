9c8d1cc8d35e5c6c845217b56bfaecd2b27e4277
Jeonwoojae <dnwo0719@icloud.com>
2024-03-11 18:50:31 +0900

    점프하기 - DP(앞으로 가면서 업데이트)
    - 기존 방식은 이전 위치의 정보로 현재 위치를 업데이트 했다면 해당 방법은 현재 위치로 앞의 데이터들을 업데이트 시킨다.

--- study_java/Backjoon11060.java
+++ study_java/Backjoon11060.java
@@ -33,4 +33,30 @@ public class Main {
         }
         System.out.println(d[n-1]);
     }
+    public static void main1(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int[] d = new int[d];
+        for (int i=0; i<n; i++) {
+            a[i] = sc.nextInt();
+            d[i] = -1;
+        }
+        d[0] = 0;
+        for (int i=0; i<n; i++) {
+            if (d[i] == -1) continue;  // 현재 위치가 방문할 수 없는 위치면 pass
+            for (int j=1; j<=a[i]; j++) {
+                // 현재 위치에서 이동할 수 있는 모든 위치에 대해 업데이트
+                if (i+j >= n) {
+                    // 범위를 넘어선다면 멈춘다.
+                    break;
+                }
+                if (d[i+j] == -1 || d[i+j] > d[i]+1) {
+                    // 해당 위치를 방문한 적이 없거나 더 짧은 방법을 찾았다면 업데이트
+                    d[i+j] = d[i] + 1;
+                }
+            }
+        }
+        System.out.println(d[n-1]);
+    }
 }
