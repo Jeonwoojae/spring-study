cb9072203d12098b4159676f4eb56d70f5c7fab9
Jeonwoojae <dnwo0719@icloud.com>
2024-06-07 22:47:21 +0900

    가운데를 말해요 - 우선순위 큐(수정)
    - 논리는 그대로지만, import 오류와 구현 부분을 더 직관적으로 수정

--- study_java/Backjoon1655.java
+++ study_java/Backjoon1655.java
@@ -1,39 +1,38 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(br.readLine());
-        // 최대 힙
-        PriorityQueue<Integer> maxPQ = new PriorityQueue<>(Comparator.reverseOrder());
-        // 최소 힙
-        PriorityQueue<Integer> minPQ = new PriorityQueue<>();
+        // 최소 힙과 최대 힙 생성
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
 
-        for(int i=0; i<N; i++) {
-            int a = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
 
-            if(maxPQ.size() == minPQ.size()) {
-                // 먼저 최대힙에 값을 넣어 중간 값이 최대힙에서 가져오도록
-                maxPQ.add(a);
-
-                // 크기를 비교하여 교환
-                if(!minPQ.isEmpty() && maxPQ.peek() > minPQ.peek()) {
-                    minPQ.add(maxPQ.poll());
-                    maxPQ.add(minPQ.poll());
-                }
-            }
-            else {
-                minPQ.add(a);
-
-                if(maxPQ.peek() > minPQ.peek()) {
-                    minPQ.add(maxPQ.poll());
-                    maxPQ.add(minPQ.poll());
-                }
+            // 최대 힙이 비어 있거나 x가 최대 힙의 루트보다 작거나 같으면, 최대 힙에 x를 추가
+            if (maxHeap.isEmpty() || x <= maxHeap.peek()) {
+                maxHeap.offer(x);
+            } else { // x가 최대 힙의 루트보다 크면 최소 힙에 x를 추가
+                minHeap.offer(x);
             }
 
-            System.out.println(maxPQ.peek());
+            // 최대 힙의 크기가 최소 힙의 크기 + 1보다 클 경우, 최대 힙의 루트를 최소 힙으로 이동
+            if (maxHeap.size() > minHeap.size() + 1) {
+                minHeap.offer(maxHeap.poll());
+            } else if (minHeap.size() > maxHeap.size()) {
+                // 최소 힙의 크기가 최대 힙의 크기보다 크면, 최소 힙의 루트를 최대 힙으로 이동
+                maxHeap.offer(minHeap.poll());
+            }
+
+            // 중간값 출력
+            sb.append(maxHeap.peek()).append("\n");
         }
+        System.out.println(sb);
     }
 }
